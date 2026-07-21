42c0a5e33ead7a4f05e4e1e68f4584746587f98d
cdesqus <cdesqus@gmail.com>
2026-07-21 14:14:41 +0700

    feat: activate users roles and approver settings

--- frontend/app/settings/roles/page.tsx
+++ frontend/app/settings/roles/page.tsx
@@ -1,4 +1,4 @@
 import { AppShell } from '../../../components/app-shell/app-shell';
-import { ModuleIndex } from '../../../components/module-index';
+import { RolesSettings } from '../../../components/settings/roles-settings';
 
-export default function RolesPage() { return <AppShell title="Roles & Permissions"><ModuleIndex title="Roles & Permissions" description="Atur role dan permission untuk setiap fungsi operasional." actionLabel="New role" columns={['Role Code','Role Name','Users','Permissions','Status','Created By']} searchPlaceholder="Search role" emptyMessage="Role bawaan sistem akan tampil setelah CRUD RBAC diaktifkan." /></AppShell>; }
+export default function RolesPage() { return <AppShell title="Roles & Permissions"><RolesSettings /></AppShell>; }
--- frontend/app/settings/users/page.tsx
+++ frontend/app/settings/users/page.tsx
@@ -1,4 +1,4 @@
 import { AppShell } from '../../../components/app-shell/app-shell';
-import { ModuleIndex } from '../../../components/module-index';
+import { UsersSettings } from '../../../components/settings/users-settings';
 
-export default function UsersPage() { return <AppShell title="Users"><ModuleIndex title="Users" description="Kelola akun username/password dan status akses aplikasi." actionLabel="New user" columns={['Username','Display Name','Roles','Status','Last Login','Created By']} searchPlaceholder="Search username or name" emptyMessage="Belum ada user tambahan selain administrator awal." /></AppShell>; }
+export default function UsersPage() { return <AppShell title="Users"><UsersSettings /></AppShell>; }
