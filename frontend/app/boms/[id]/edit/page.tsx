866a67d5d92696871e9642f462dc52a9c04174f1
cdesqus <cdesqus@gmail.com>
2026-09-10 13:25:28 +0700

    feat: make BOM drafts editable and clearer

---
+++ frontend/app/boms/[id]/edit/page.tsx
@@ -0,0 +1,3 @@
+import {BOMForm} from '../../../../components/bom/bom-form';
+import {AppShell} from '../../../../components/app-shell/app-shell';
+export default function EditBOMPage(){return <AppShell title="Edit Draft BOM"><BOMForm/></AppShell>}
